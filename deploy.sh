eb713ea915e4cc1b57172def9d3d6920b6db85d2
cjfff <cjfff1996@gmali.com>
2020-10-21 11:34:59 +0800

    ci: update ci

--- deploy.sh
+++ deploy.sh
@@ -6,13 +6,14 @@ set -e
 # 生成静态文件
 npm run build
 
-
 mkdir temp_web
 cd temp_web
 
+# 拷贝 gh-pages 目录下到
 git clone --depth 1 -b gh-pages --single-branch https://${GITHUB_TOKEN}@github.com/cjfff/xmind-analysis.git && cd /xmind-analysis
 
-rm -rf ./xmind-analysis/*
+# 删除原本的所有文件
+rm -rf ./*
 
 cp -rf ../../dist/* .
 
